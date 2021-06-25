1a104203635cbfdf98c8c65e1364933f1e1a19b2
Juan Fran Martin <juanframaro@gmail.com>
2021-06-25 22:25:50 +0100

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
-
+var moment = require('moment'); 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -25,8 +25,38 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  //var momento = moment(parseInt(req.params.date)).valueOf();
+  var dateUrl = req.params.date; 
+  console.log(dateUrl);
+  if(dateUrl.match(/\d{5,}/)){
+    dateX = new Date(parseInt(dateUrl));
 
-// listen for requests :)
+  } else{
+    dateX = new Date(dateUrl);
+  }
+
+  if (dateX == "Invalid Date"){
+    res.json({
+      error: "Invalid Date"
+    });
+  } else {
+    res.json({
+      unix: dateX.getTime(),
+      utc: dateX.toUTCString()
+    });
+  }
+});
+
+
+app.get("/api/", function (req, res) {
+  res.json({
+    unix: Date.now(),
+    utc: new Date( Date.now() ).toUTCString()
+  });
+});
+
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
