28920f66cd458bf0f5f52b367fc9a31486a94dfe
Juan Fran Martin <juanframaro@gmail.com>
2021-06-25 23:08:13 +0100

    moved to timestamp_microservice

--- timestamp_microservice/server.js
+++ timestamp_microservice/server.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-var moment = require('moment'); 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -27,9 +26,7 @@ app.get("/api/hello", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/:date", function (req, res) {
-  //var momento = moment(parseInt(req.params.date)).valueOf();
   var dateUrl = req.params.date; 
-  console.log(dateUrl);
   if(dateUrl.match(/\d{5,}/)){
     dateX = new Date(parseInt(dateUrl));
 
